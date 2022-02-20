081806d3e0b1f00ca69e49ae6f8e7e6ec0b58113
Андрей <slhr174@gmail.com>
2022-02-20 15:59:38 +0500

    adding commented content

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import {Avatar, Card, CardContent, CardHeader, CardMedia, Divider, Typography} from "@mui/material";
+import defaultAvatar from "../../assets/images/defaultAvatar.png";
+import contentImage from "../../assets/images/content.jpg";
+
 
 const Content = () => {
     return (
-        <div>
-            Content
-        </div>
+        <Card variant="outlined">
+
+            <CardHeader avatar={
+                <Avatar alt="User Avatar" src={defaultAvatar}/>
+            }
+                        title="John Doe"
+                        subheader={"1 day ago"}/>
+
+            <Divider/>
+
+            <CardContent>
+
+                <Typography>
+                    «Две книги по C++ для чайников. Смирительная рубашка в подарок».
+                </Typography>
+
+            </CardContent>
+
+            <CardMedia component="img"
+                       image={contentImage}
+                       alt="Content Image"/>
+
+        </Card>
     );
 };
 
