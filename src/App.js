8cb915f8857b0680fd5d062cd1be7d34095cf8cc
Андрей <slhr174@gmail.com>
2022-02-20 15:24:29 +0500

    adding dependencies, structure, global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import {createGlobalStyle} from "styled-components";
+import {Container} from "@mui/material";
+import Content from "./components/Content/Content";
+import Comments from "./components/Comments/Comments";
+
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #f2f2f2;
+    font-size: 13px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
 
 const App = () => {
     return (
-        <div>
-            app
-        </div>
+        <Container maxWidth="md" sx={{p: 3}}>
+            <Content/>
+            <Comments/>
+            <GlobalStyles/>
+        </Container>
     );
-}
+};
+
 
 export default App;
